70e9a817dbde8a90d97bf8a0a94100fce7a09d51
Elif <elif@Elifs-MacBook-Pro.local>
2023-08-06 19:38:50 +0300

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <TodoList />
+  </div>
+</template>
+
+<script>
+import TodoList from '~/components/TodoList.vue';
+
+export default {
+  components: {
+    TodoList,
+  },
+  data() {
+    return {
+      isViewingCompleted: false,
+    };
+  },
+  mounted() {
+    this.$store.dispatch('todos/loadTodos');
+  },
+};
+</script>
